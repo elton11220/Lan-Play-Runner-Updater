a1f8a4b9d9c8225015619ba8f593c5af27eedd81
Elton11220 <jonesbak@outlook.com>
2020-07-16 19:26:16 +0800

    添加项目文件。

---
+++ Lan-Play-Runner-Updater/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Lan_Play_Runner_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(127, 150);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(60, 49);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Visible = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(138, 83);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(133, 25);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "服务器版本：1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 147);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Lan-Play-Runner Updater";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ Lan-Play-Runner-Updater/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using MetroFramework.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Lan_Play_Runner_Updater
+{
+    public partial class Form1 : MetroForm 
+    {
+        private static string Server = "https://elton1122.top/App/status.html";
+        private static string Down_Url = "";
+        private static int Server_Version = 0;
+        private static int Local_Version = 0;
+        private static int Local_UpdaterVer = 2;
+        public static string me_path = System.IO.Directory.GetCurrentDirectory(); //程序路径
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetVersion(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                System.Diagnostics.FileVersionInfo fv = System.Diagnostics.FileVersionInfo.GetVersionInfo(path);
+                string ver = fv.FileVersion;
+                string ver1 = ver.Substring(0, 1);
+                Local_Version = Convert.ToInt32(ver1);
+            }
+            else
+            {
+                MessageBox.Show("文件目录损坏，请重新下载", "错误");
+                Application.Exit();
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string status_page = webBrowser1.Document.Body.InnerHtml;
+            JObject obj = JObject.Parse(status_page);
+            Server_Version = (int)obj["version"];
+            Down_Url = (string)obj["updurl"];
+            int Down_Ver = (int)obj["updaterver"];
+            if(Local_UpdaterVer <= Down_Ver)
+            {
+                //
+            }
+            else
+            {
+                MessageBox.Show("服务器版本为重大更新，请重新下载软件", "提示");
+                Application.Exit();
+                return;
+            }
+            metroLabel1.Text = "服务器版本：" + Convert.ToString(Server_Version);
+            GetVersion(me_path + "\\Lan-Play-Runner.exe");
+            if (Local_Version < Server_Version)
+            {
+                MessageBox.Show("检测到新版本：Version " + Server_Version + ".0.0", "注意");
+                System.Diagnostics.Process.Start(Down_Url);
+                Thread.Sleep(2000);
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("无需更新", "错误");
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(Server);
+        }
+    }
+}
